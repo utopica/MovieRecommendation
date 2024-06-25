079619db8b74927d279d6fca99811d98daa3ccd5
Elif OKUMUŞ <98635931+utopica@users.noreply.github.com>
2024-06-25 16:23:01 +0300

    [feat] Added dbcontext

---
+++ Infrastructure/MovieRecommendation.Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovieRecommendation.Domain.Entities;
+
+namespace MovieRecommendation.Persistence.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            // Properties
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+            builder.Property(u => u.Auth0Id)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.HasIndex(u => u.Auth0Id)
+                .IsUnique();
+
+            builder.HasMany(u => u.Ratings)
+                .WithOne(r => r.User)
+                .HasForeignKey(r => r.UserId);
+
+            builder.HasMany(u => u.Recommendations)
+                .WithOne(r => r.User)
+                .HasForeignKey(r => r.UserId);
+
+            // COMMON FIELDS
+
+            // CreatedByUserId
+            builder.Property(x => x.CreatedByUserId).IsRequired();
+            builder.Property(x => x.CreatedByUserId).HasMaxLength(75);
+
+            // CreatedOn
+            builder.Property(x => x.CreatedOn).IsRequired();
+
+            // ModifiedByUserId
+            builder.Property(x => x.ModifiedByUserId).IsRequired(false);
+            builder.Property(x => x.ModifiedByUserId).HasMaxLength(75);
+
+            // LastModifiedOn
+            builder.Property(x => x.ModifiedOn).IsRequired(false);
+
+            // DeletedByUserId
+            builder.Property(x => x.DeletedByUserId).IsRequired(false);
+            builder.Property(x => x.DeletedByUserId).HasMaxLength(75);
+
+            // DeletedOn
+            builder.Property(x => x.DeletedOn).IsRequired(false);
+
+            // IsDeleted
+            builder.Property(x => x.IsDeleted).IsRequired();
+
+            builder.ToTable("Users");
+        }
+
+    }
+}
---
+++ Infrastructure/MovieRecommendation.Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieRecommendation.Domain.Entities;
+
+namespace MovieRecommendation.Persistence.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Recommendation> Recommendations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
